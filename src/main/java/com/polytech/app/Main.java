b22d1fd6b74aad51d4bb92072cbc4e25ea6bcc2b
Lucas <lucaslelaidier@gmail.com>
2019-10-22 15:56:30 +0200

    foget to commit last week

--- src/main/java/com/polytech/app/Main.java
+++ src/main/java/com/polytech/app/Main.java
@@ -1,14 +1,18 @@
 package com.polytech.app;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
+import org.bytedeco.opencv.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
-public class Main extends Application {
-	@Override
+
+public class Main /*extends Application*/ {
+	/*@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = FXMLLoader.load(getClass().getResource("/vues.fxml"));
@@ -20,9 +24,15 @@ public class Main extends Application {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 	public static void main(String[] args) {
-		launch(args);
+		// launch(args);
+
+		Mat image = imread("D:\\Lucas\\Pictures\\11402933_845908588820714_5708211936915400943_n.jpg");
+		if (image != null) {
+			GaussianBlur(image, image, new Size(51, 51), 0);
+			imwrite("D:\\Lucas\\Pictures\\blured-bg.jpg", image);
+		}
 	}
 }
