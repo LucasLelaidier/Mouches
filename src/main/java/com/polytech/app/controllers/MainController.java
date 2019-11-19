6ed262c6841fa4d4dab4df8a88a93ae1348b7dae
Lucas Lelaidier <lucaslelaidier@gmail.com>
2019-11-19 21:02:08 +0100

    detecting algorithms from xml file

--- src/main/java/com/polytech/app/controllers/MainController.java
+++ src/main/java/com/polytech/app/controllers/MainController.java
@@ -1,14 +1,20 @@
 package com.polytech.app.controllers;
 
 import com.polytech.app.algorithm.*;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
@@ -18,6 +24,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class MainController {
 	@FXML
@@ -28,6 +39,9 @@ public class MainController {
 
 	@FXML
 	private TextField treshold;
+
+	@FXML
+    private Menu algorithmMenu;
 
 	private String image;
 
@@ -53,7 +67,37 @@ public class MainController {
                 imageAnalisee.setImage(fxImage);
             }
         });
-	}
+
+        // A remettre dans un controlleur dédié
+
+        // Getting factory used to create XML parser
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try {
+            // Creating parser
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+            // Opening the file containing the algorithm list
+            final Document document = builder.parse(getClass().getResource("/algorithms.xml").openStream());
+            // Getting root element
+            final Element racine = document.getDocumentElement();
+            // Getting childs of root
+            final NodeList racineNoeuds = racine.getChildNodes();
+
+            final int nbRacineNoeuds = racineNoeuds.getLength();
+            for (int i = 0; i < nbRacineNoeuds; i++)
+            {
+                // For each childs if its a node (an algorithm) we add its name to algorithm menu
+                if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE)
+                {
+                    final Element algo = (Element) racineNoeuds.item(i);
+                    algorithmMenu.getItems().add(new MenuItem(algo.getAttribute("name")));
+                }
+            }
+        }
+        catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	@FXML
 	private void ouvrirImage() {
