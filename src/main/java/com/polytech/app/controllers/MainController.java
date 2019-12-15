e774507405ea8ef75b64284f3d712a962f4cfb44
Lucas Lelaidier <lucaslelaidier@gmail.com>
2019-12-15 18:11:54 +0100

    some minors changes

--- src/main/java/com/polytech/app/controllers/MainController.java
+++ src/main/java/com/polytech/app/controllers/MainController.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +22,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -95,25 +93,7 @@ public class MainController {
             // Getting childs of root
             final NodeList racineNoeuds = racine.getChildNodes();
 
-            final int nbRacineNoeuds = racineNoeuds.getLength();
-            for (int i = 0; i < nbRacineNoeuds; i++)
-            {
-                // For each childs if its a node (an algorithm) we add its name to algorithm menu
-                if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE)
-                {
-                    final Element algo = (Element) racineNoeuds.item(i);
-                    MenuItem menuItem = new MenuItem(algo.getAttribute("name"));
-                    algorithmMenu.getItems().add(menuItem);
-
-                    String path = algo.getAttribute("path");
-                    Class algorithmClass = Class.forName(path);
-                    Constructor constructor = algorithmClass.getConstructor();
-
-                    Algorithm algorithmInstance = (Algorithm) constructor.newInstance();
-
-                    menuItem.setOnAction(event -> choosedAlgorithm = algorithmInstance);
-                }
-            }
+            instanciateAlgoMenu(racineNoeuds);
         }
         catch (ParserConfigurationException | SAXException | IOException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -161,6 +141,62 @@ public class MainController {
             } else {
                 Image fxImage = new Image("file:" + edgedImage);
                 imageAnalisee.setImage(fxImage);
+            }
+        }
+    }
+
+    public void instanciateAlgoMenu(NodeList racineNoeuds) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        final int nbRacineNoeuds = racineNoeuds.getLength();
+        for (int i = 0; i < nbRacineNoeuds; i++)
+        {
+            // For each childs if its a node (an algorithm) we add its name to algorithm menu
+            if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE)
+            {
+                final Element algo = (Element) racineNoeuds.item(i);
+                MenuItem menuItem = new MenuItem(algo.getAttribute("name"));
+                algorithmMenu.getItems().add(menuItem);
+
+                String path = algo.getAttribute("path");
+                Class algorithmClass = Class.forName(path);
+                Constructor constructor = algorithmClass.getConstructor();
+
+                Algorithm algorithmInstance = (Algorithm) constructor.newInstance();
+
+                updateParameters(algo.getChildNodes(), menuItem, algorithmInstance);
+            }
+        }
+    }
+
+    public void updateParameters(NodeList parameters, MenuItem menuItem, Algorithm algorithmInstance) {
+        for (int i = 0; i < parameters.getLength(); i++) {
+            // We search for the parameters node
+            if(parameters.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                if(parameters.item(i).getNodeName().equals("parameters")) {
+                    final NodeList parametersList = parameters.item(i).getChildNodes();
+                    // Once we found it we parse all parameter node
+                    // and add the corresponding fx element to the gridpane
+
+                    int row = 1; // The row where we start to add parameters
+                    for (int j = 0; j < parametersList.getLength(); j++) {
+                        // Only cast elements else an error is raised
+                        if(parametersList.item(j).getNodeType() == Node.ELEMENT_NODE) {
+                            Element param = (Element) parametersList.item(j);
+                            if(param.getAttribute("type").equals("slider")) {
+                                String range = param.getAttribute("range");
+                                String[] ranges = range.split("-");
+                                int min = Integer.parseInt(ranges[0]);
+                                int max = Integer.parseInt(ranges[1]);
+
+                                menuItem.setOnAction(event -> {
+                                    sliderZoom.setMin(min);
+                                    sliderZoom.setMax(max);
+                                    sliderZoom.setMajorTickUnit(Math.round(max / 4));
+                                    choosedAlgorithm = algorithmInstance;
+                                });
+                            }
+                        }
+                    }
+                }
             }
         }
     }
